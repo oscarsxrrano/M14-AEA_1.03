44656d8b5fac3c62a3f294405579a4611b64b147
oscarsxrrano <oscar.serrano.7e5@itb.cat>
2024-10-01 18:27:04 +0200

    Add Files

---
+++ AEA_1.03.js
@@ -0,0 +1,19 @@
+const addTask =() => {
+    const { value: taskInput } = document.getElementById( "task");
+    if (taskInput.trim() === "") return;
+
+    const taskList = document.getElementById("listTasks");
+    const li = document.createElement("li");
+    
+    li.className = "liClass";
+    li.innerHTML = '${taskInput} <button class="deleteClass">Delete</button>';
+
+    const deleteButton = li.querySelector(".deleteClass");
+    deleteButton.onclick = () => {
+        taskList.removeChild(li);
+    }; 
+    
+    taskList.appendChild(li);
+
+    document.getElementById("task").value = ""; 
+};
